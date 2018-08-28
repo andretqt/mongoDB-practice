949dd72717269c188f9a666c75947e3ac2b8cf9b
yung dev <andre.torquati@gmail.com>
2018-08-28 17:37:13 -0400

    scraping seems to work on backend. have to send result to front end now

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 var express = require("express");
 var bodyParser = require("body-parser");
 var cheerio = require("cheerio");
-var exphbs = require("express-handlebars")
+var axios = require("axios");
+var exphbs = require("express-handlebars");
+var request = require("request");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.Promise = Promise;
@@ -24,17 +26,38 @@ app.use(express.static("public"));
 app.engine(
     "handlebars",
     exphbs({
-      defaultLayout: "main"
+        defaultLayout: "main"
     })
-  );
+);
 app.set("view engine", "handlebars");
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
 
 //doing the routing here below
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.render("homepage");
 });
 
-app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+app.get("/scrape", function (req, res) {
+    console.log("routing seems to work..let's send a status response for now");
+    //do the scraping in here
+    request("http://www.echojs.com/", function (error, response, html) {
+        var $ = cheerio.load(html);
+        $("article h2").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+            console.log(result);
+        });
+    });
+
 });
+
